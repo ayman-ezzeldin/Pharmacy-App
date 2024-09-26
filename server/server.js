f02951518aa227792ba0edf7f9a702eb1b385734
Ayman ezz <aymanmae12@gmail.com>
2024-09-26 18:36:32 +0300

    back to address

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
 
 
 mongoose
@@ -44,6 +45,7 @@ app.use('/api/auth',authRouter)
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
 
 
 app.listen(PORT, () => {
