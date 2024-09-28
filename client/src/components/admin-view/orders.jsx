db7116eae4e2f696a5f4c0bbbd1c46782eb6c5ec
Ayman ezz <aymanmae12@gmail.com>
2024-09-28 20:13:17 +0300

    order-details ui

--- client/src/components/admin-view/orders.jsx
+++ client/src/components/admin-view/orders.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
 import { Card, CardContent, CardHeader, CardTitle } from "../../components/ui/card"
-import { Table, TableHead, TableHeader, TableRow } from '../../components/ui/table'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../components/ui/table'
+import { Button } from "../ui/button"
+import { Dialog } from "../ui/dialog"
+import AdminOrderDetailsView from "./order-details"
+
 const AdminOrdersView = () => {
+
+  const [openDetailsDialog, setOpenDetailsDialog] = useState(false)
+
   return (
-    <Card>
+    <Card className=" bg-white" >
       <CardHeader>
         <CardTitle>
           All Orders 
@@ -21,6 +29,22 @@ const AdminOrdersView = () => {
               </TableHead>
             </TableRow>
           </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell> 1234 </TableCell>
+              <TableCell> 24/5/2022 </TableCell>
+              <TableCell> In Process </TableCell>
+              <TableCell> $ 1000 </TableCell>
+              <TableCell> 
+                <Dialog open={openDetailsDialog} onOpenChange={setOpenDetailsDialog} >
+                  <Button 
+                    onClick={() => setOpenDetailsDialog(true)}
+                    className="bg-black text-white hover:bg-black rounded-xl hover:text-white" >View Details</Button>
+                    <AdminOrderDetailsView />
+                </Dialog>
+              </TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
       </CardContent>
     </Card>
