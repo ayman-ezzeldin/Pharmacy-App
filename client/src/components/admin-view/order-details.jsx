96d7365635ce0305c0755fededf76b6b98bfa052
Ayman ezz <aymanmae12@gmail.com>
2024-09-30 18:32:30 +0300

    order status done

--- client/src/components/admin-view/order-details.jsx
+++ client/src/components/admin-view/order-details.jsx
@@ -3,65 +3,140 @@ import { Label } from "../ui/label";
 import { Separator } from "../ui/separator";
 import CommonForm from "../common/form";
 import { useState } from "react";
+import { Badge } from "../ui/badge";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllOrdersForAdmin, getOrderDetailsForAdmin, updateOrderStatus } from "../../store/admin/order-slice";
+import { useToast } from "../../hooks/use-toast";
 
 const initialFormData = {
   status: '',
 };
 
-const AdminOrderDetailsView = () => {
+const AdminOrderDetailsView = ({ orderDetails }) => {
 
   const [formData, setFormData] = useState(initialFormData);
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const { toast } = useToast()
 
   function handleUpdateStatus(event) {
     event.preventDefault();
+    const { status } = formData;
+    
+    if (status.trim().length === 0) {
+      toast({
+        title: "All fields are required",
+      });
+      return;
+    }
+    
+    dispatch(updateOrderStatus({ id : orderDetails._id, orderStatus: status})).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(getOrderDetailsForAdmin(orderDetails?._id));
+        dispatch(getAllOrdersForAdmin());        
+        setFormData(initialFormData);
+        toast({
+          title: "Order status updated successfully",
+        });
+      }
+    });
   }
 
   return (
-    <DialogContent className=" bg-white sm:max-w-[600px]">
+    <DialogContent className=" bg-white sm:max-w-[600px] max-h-[95vh] overflow-y-auto sm:rounded-2xl ">
       <div className="grid gap-4">
+      <div className="grid gap-6">
         <div className="grid gap-2">
           <div className="flex mt-6 items-center justify-between">
-            <p className="font-medium">Order ID</p>
-            <Label>2323</Label>
+            <p>Order ID</p>
+            <Label>{orderDetails?._id}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium">Order Date</p>
-            <Label>23/4/2203</Label>
+            <p>Order Date</p>
+            <Label>{orderDetails?.orderDate.split("T")[0]}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium">Order Price</p>
-            <Label>$ 299</Label>
+            <p>Order Price</p>
+            <Label>${orderDetails?.totalAmount}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium">Order Status</p>
-            <Label>In Process</Label>
+            <p>Payment method</p>
+            <Label>{orderDetails?.paymentMethod}</Label>
+          </div>
+          <div className="flex mt-2 items-center justify-between">
+            <p>Payment Status</p>
+            <Label>{orderDetails?.paymentStatus}</Label>
+          </div>
+          <div className="flex mt-2 items-center justify-between">
+            <p>Order Status</p>
+            <Label>
+              <Badge
+                className={`py-1 px-3 text-white ${
+                  orderDetails?.orderStatus === "confirmed"
+                    ? "bg-green-500 hover:bg-green-600 "
+                    : orderDetails?.orderStatus === "rejected"
+                    ? "bg-red-600 hover:bg-red-700 "
+                    : "bg-black hover:bg-black "
+                }`}
+              >
+                {orderDetails?.orderStatus}
+              </Badge>
+            </Label>
           </div>
         </div>
-        <Separator className=" w-full bg-gray-200 h-0.5" />
+        <Separator />
         <div className="grid gap-4">
           <div className="grid gap-2">
             <div className="font-bold">Order Details</div>
             <ul className="grid gap-3">
-              <li className="flex items-center justify-between">
-                <span>Product One</span>
-                <span>$ 139</span>
-              </li>
+              {orderDetails?.cartItems && orderDetails?.cartItems.length > 0
+                ? orderDetails?.cartItems.map((item) => (
+                    <li
+                      key={item._id}
+                      className="flex items-center justify-between"
+                    >
+                      <span>Title: {item.title}</span>
+                      <span>Quantity: {item.quantity}</span>
+                      <span>Price: ${item.price}</span>
+                    </li>
+                  ))
+                : null}
             </ul>
           </div>
         </div>
         <div className="grid gap-4">
           <div className="grid gap-2">
-            <div className="font-medium">Shipping Info</div>
+            <div className="font-bold">Shipping Info</div>
             <div className="grid gap-0.5 text-muted-foreground">
-              <span>Ayman ezz</span>
-              <span>Address</span>
-              <span>City</span>
-              <span>Pincode</span>
-              <span>Phone</span>
-              <span>Notes</span>
+              <div className="flex mt-2 items-center justify-between">
+                <p>Name</p>
+                <Label>{user.username}</Label>
+              </div>
+              <div className="flex items-center justify-between mt-2">
+                <p>Address</p>
+                <Label> {orderDetails?.addressInfo?.address}</Label>
+              </div>
+              <div className="flex items-center justify-between mt-2">
+                <p>City</p>
+                <Label> {orderDetails?.addressInfo?.city}</Label>
+              </div>
+              <div className="flex items-center justify-between mt-2">
+                <p>Pincode</p>
+                <Label> {orderDetails?.addressInfo?.pincode}</Label>
+              </div>
+              <div className="flex items-center justify-between mt-2">
+                <p>Phone</p>
+                <Label> {orderDetails?.addressInfo?.phone}</Label>
+              </div>
+              <div className="flex items-center justify-between mt-2">
+                <p>Notes</p>
+                <Label> {orderDetails?.addressInfo?.notes}</Label>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+
         <div>
           <CommonForm
             formControls={[
--- client/src/components/admin-view/orders.jsx
+++ client/src/components/admin-view/orders.jsx
@@ -1,20 +1,37 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Card, CardContent, CardHeader, CardTitle } from "../../components/ui/card"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../components/ui/table'
 import { Button } from "../ui/button"
-import { Dialog } from "../ui/dialog"
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "../ui/dialog"
 import AdminOrderDetailsView from "./order-details"
+import { Badge } from "../ui/badge"
+import { useDispatch, useSelector } from "react-redux"
+import { getAllOrdersForAdmin, getOrderDetailsForAdmin, resetOrderDetails } from "../../store/admin/order-slice"
 
 const AdminOrdersView = () => {
 
   const [openDetailsDialog, setOpenDetailsDialog] = useState(false)
+  const dispatch = useDispatch();
+  const { orderList, orderDetails } = useSelector((state) => state.adminOrder);
+
+  function handleFetchOrderDetails(getId) {
+    dispatch(getOrderDetailsForAdmin(getId));
+  }
+
+
+  useEffect(() => {
+    dispatch(getAllOrdersForAdmin())
+  }, [dispatch])
+
+  useEffect(() => {
+    if (orderDetails !== null) setOpenDetailsDialog(true);
+  }, [orderDetails])
+  
 
   return (
-    <Card className=" bg-white" >
+    <Card>
       <CardHeader>
-        <CardTitle>
-          All Orders 
-        </CardTitle>
+        <CardTitle>Order History</CardTitle>
       </CardHeader>
       <CardContent>
         <Table>
@@ -24,26 +41,60 @@ const AdminOrdersView = () => {
               <TableHead>Order Date</TableHead>
               <TableHead>Order Status</TableHead>
               <TableHead>Order Price</TableHead>
-              <TableHead >
-                <span className="sr-only" >Details</span>
+              <TableHead>
+                <span className="sr-only">Details</span>
               </TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell> 1234 </TableCell>
-              <TableCell> 24/5/2022 </TableCell>
-              <TableCell> In Process </TableCell>
-              <TableCell> $ 1000 </TableCell>
-              <TableCell> 
-                <Dialog open={openDetailsDialog} onOpenChange={setOpenDetailsDialog} >
-                  <Button 
-                    onClick={() => setOpenDetailsDialog(true)}
-                    className="bg-black text-white hover:bg-black rounded-xl hover:text-white" >View Details</Button>
-                    <AdminOrderDetailsView />
-                </Dialog>
-              </TableCell>
-            </TableRow>
+            {orderList && orderList.length > 0
+              ? orderList.map((orderItem) => (
+                  <TableRow key={orderItem?._id} >
+                    <TableCell>{orderItem?._id}</TableCell>
+                    <TableCell>{orderItem?.orderDate.split("T")[0]}</TableCell>
+                    <TableCell>
+                      <Badge
+                        className={`py-1 px-3 text-white ${
+                          orderItem?.orderStatus === "confirmed"
+                            ? "bg-green-500 hover:bg-green-600 "
+                            : orderItem?.orderStatus === "rejected"
+                            ? "bg-red-600 hover:bg-red-700 "
+                            : "bg-black hover:bg-black "
+                        }`}
+                      >
+                        {orderItem?.orderStatus}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>${orderItem?.totalAmount}</TableCell>
+                    <TableCell>
+                      <Dialog
+                        open={openDetailsDialog}
+                        onOpenChange={() => {
+                          setOpenDetailsDialog(false);
+                          dispatch(resetOrderDetails());
+                        }}
+                      >
+                        <DialogTitle className="hidden" >Order Details</DialogTitle>
+                          <DialogTrigger>
+                            <Button
+                            onClick={() =>
+                              handleFetchOrderDetails(orderItem?._id)
+                            }
+                            className="bg-black text-white hover:bg-black rounded-xl hover:text-white"
+                          >
+                            View Details
+                          </Button>
+                          </DialogTrigger>
+                          <DialogContent className=" bg-white sm:max-w-[600px] overflow-hidden sm:rounded-2xl " >
+                            <AdminOrderDetailsView
+                              orderDetails={orderDetails}
+                            />
+                          </DialogContent>
+                      </Dialog>
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
           </TableBody>
         </Table>
       </CardContent>
