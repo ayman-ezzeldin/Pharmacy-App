86a6668c3adef9ad9eaf257560186bd17f2e37ba
Ayman ezz <aymanmae12@gmail.com>
2024-11-23 13:00:06 +0200

    small edit

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -42,7 +42,7 @@ const AuthRegister = () => {
   }
   
   return (
-    <div className="ms-auto w-full max-w-md space-y-6">
+    <div className="mx-auto w-full max-w-md space-y-6">
       <div className=" text-center">
         <h1 className=" text-3xl font-bold tracking-tight text-foreground">
           Create new account
